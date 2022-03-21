7c4b4b779cb0b20fa53041a93535dc5bbc99de18
christian-2009 <c-sophocleous@hotmail.com>
2022-03-21 12:15:04 +0000

    used prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import PageHeader from './components/PageHeader'
+import PageHeader from "./components/PageHeader";
 
 function App(): JSX.Element {
   return (
     <>
       <PageHeader />
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,8 +1,8 @@
 export default function PageHeader(): JSX.Element {
-    return (
-        <>
-            <h1>Fitness Goals</h1>
-            <p>Enjoy features that will help you on your fitness journey</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Fitness Goals</h1>
+      <p>Enjoy features that will help you on your fitness journey</p>
+    </>
+  );
 }
