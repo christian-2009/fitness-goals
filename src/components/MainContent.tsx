61218c562599475205590a1d8f12413741a3a929
Christian Sophocleous <christiansophocleous@Christians-MacBook-Pro.local>
2022-04-20 15:32:11 +0100

    small change to see how to use github on laptop

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function MainContent(): JSX.Element {
     const [weightArray, setWeightArray] = useState<string[]>([])
