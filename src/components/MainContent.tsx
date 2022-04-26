8a3ede1d57e82c98f4dc39a400e1536bd9b749c3
Christian Sophocleous <christiansophocleous@christians-mbp.home>
2022-04-26 20:21:03 +0100

    formatted with prettier

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,33 +1,86 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface weightData {
+  weight: string;
+  id: string;
+  dates: Date;
+}
 
 export default function MainContent(): JSX.Element {
-    const [weightArray, setWeightArray] = useState<string[]>([])
-    const [text, setText] = useState('')
+  const [weightArray, setWeightArray] = useState<string[]>([]);
+  const [weightArrayOfObjects, setWeightArrayOfObjects] = useState<
+    weightData[]
+  >([]);
+  const [text, setText] = useState("");
 
-    const handleWeight = () => {
-        setWeightArray([...weightArray, text])
-    }
+  //adding the weight to database
+  const handleAddWeight = () => {
+    const data = { weight: text };
+    axios.post("http://localhost:4000/weights", data);
+    setText("");
+    setWeightArray([...weightArray, text]);
+  };
 
-    const displayWeights = weightArray.map((weight, index) => {
-        return (
-             <li key={index}>{weight + 'kg'}</li>
-        )
-     })
-    
+  // const handleDeleteWeight = () => {
+  //   const filteredWeightArray = weightArray.filter((object.id) => {
+
+  //   })
+  // }
+
+  //fetching the weights from server
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get("http://localhost:4000/weights").then((response) => {
+        const weightData: weightData[] = response.data;
+        setWeightArrayOfObjects(weightData);
+      });
+    };
+    fetchData();
+  }, [weightArray]);
+
+  //mapping over weight objects to display onscreen
+  const displayWeights = weightArrayOfObjects.map((object) => {
+    const currentDate = new Date(object.dates).toLocaleDateString();
     return (
-      <>
-      <div className='weight-enter-display'>
-        <input
-          placeholder="enter weight..."
-          value={text}
-          onChange={(event) => {
-            setText(event.target.value);
-          }}
-        />
-        <button onClick = {handleWeight}>Enter</button>
-
-        <ul className='weight-list'>{displayWeights.slice(Math.max(0, displayWeights.length-10),)}</ul>
+      <div key={object.id}>
+        <li className="individual-weight">
+          {object.weight + "kg " + currentDate}
+        </li>
+        <div className="center-delete">
+          {/* need to fix the delete function */}
+          {/* <button className='individual-weight delete-button' onClick = {() => {
+               const filteredWeightArray = weightArray.filter((obj) =>{
+                 obj.id === object.id
+               })
+             }}>delete</button> */}
+        </div>
       </div>
-      </>  
-    )
+    );
+  });
+
+  return (
+    <>
+      <div className="form-style">
+        <h2 className="weight-title">Weigh in</h2>
+        <div className="input-button">
+          <input
+            className="enter-weight"
+            placeholder="weight..."
+            value={text}
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+          />
+          <button className="weight-button" onClick={handleAddWeight}>
+            Enter
+          </button>
+        </div>
+
+        <ul className="weight-list">
+          {displayWeights.slice(Math.max(0, displayWeights.length - 10))}
+        </ul>
+      </div>
+    </>
+  );
 }
